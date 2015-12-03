18cb4aacf5cbddd6e15dc2a1ef6254e10930ae58
Sasikala-Ravichandran <passion.rails@gmail.com>
2015-12-03 12:01:05 -0600

    modified tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.find_by(email: params[:session][:email])
-    puts "sasi"
   	if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_back_or user
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,13 +2,36 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
-   test 'layout links' do
+  def setup
+  	@user = users(:example)
+  end
+
+   test 'layout links before log in' do
      get root_path
      assert_template 'static_pages/home'
      assert_select "a[href=?]", root_path, count: 2
      assert_select "a[href=?]", about_path
      assert_select "a[href=?]", help_path
      assert_select "a[href=?]", contact_path
-     get signup_path
+     assert_select "a[href=?]", login_path
+     assert_select "a[href=?]", signup_path
+     get root_path
+     assert_template 'static_pages/home'
+     log_in_as(@user) 
+     get user_path(@user)
+     assert_template 'users/show'
+     assert_select "a[href=?]", root_path, count: 2
+     assert_select "a[href=?]", about_path
+     assert_select "a[href=?]", help_path
+     assert_select "a[href=?]", contact_path
+     assert_select "a[href=?]", users_path
+     assert_select "a[href=?]", user_path(@user)
+     assert_select "a[href=?]", edit_user_path(@user)
+     assert_select "a[href=?]", logout_path
+    end
+
+    test 'layout links after logged in' do
+     
+
     end
 end
